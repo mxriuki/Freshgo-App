ae8d70f0a35cd18f5772d853e11ecd615f2f13d2
mxriuki <mwendav37@gmail.com>
2024-09-17 12:12:37 +0300

    I installed react-router,...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 
+import './App.css';
+// import '././components/style.css'
+import { Route, Routes } from 'react-router-dom';
+import RouterPage from '../src/components/RouterPage';
+// import Cart from './components/cart/Cart'
+import { useState } from 'react';
+// import Buy from './components/AfterPurchase/Buy';
+// import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
 
 function App() {
+  const[info,setInfo]=useState([])
+  const handleInfo=(item)=>{
+    setInfo(item)
+  }
   return (
-    <div className="text-blue-500">
-      <h1>Hello Vite + React!</h1>
+    <div className="overflow-x-hidden">
+      <ToastContainer position="top-center" autoClose={3000} />
+      <Routes>
+        <Route path='/' element={<RouterPage handleInfo={handleInfo} info={info}/>} />
+        {/* <Route path='cart' element={<Cart info={info} setInfo={setInfo} />} />         */}
+        {/* <Route path='buy' element={<Buy/>} />       */}
+      </Routes>
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
