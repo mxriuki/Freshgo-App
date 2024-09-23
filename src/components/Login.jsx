ea90594850cc9a75fd7ae19a2e0082038dac665e
eustachekamala <eustachejared25@gmail.com>
2024-09-23 14:59:03 +0300

    added the toast react reaction and add the toogle button to the password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,39 +1,59 @@
-// src/Login.jsx
 import { Link } from 'react-router-dom';
 import GoogleIcon from '@mui/icons-material/Google';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../config/firebase-config';
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
-  // Handle email/password login
   const handleEmailLogin = async (e) => {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      alert("Logged in successfully");
-      navigate('/home')
+      toast.success("Logged in successfully");
+      navigate('/home');
     } catch (error) {
-      alert(`Login failed: ${error.message}`);
+      console.log(error);
+      toast.error("Login failed", { duration: 2000 });
     }
   };
 
-  // Handle Google login
   const handleGoogleLogin = async (e) => {
     e.preventDefault();
     try {
       const result = await signInWithPopup(auth, new GoogleAuthProvider());
       const user = result.user;
       console.log(user);
-      alert("Logged in successfully");
-      navigate('/home')
+      toast.success("Logged in successfully");
+      navigate('/home');
     } catch (error) {
-      alert(`Login failed: ${error.message}`);
+      console.log(error);
+      toast.error("Login failed", { duration: 2000 });
+    }
+  };
+
+  const handleEmailError = (e) => {
+    if (e.target.validity.typeMismatch) {
+      toast.error("Please enter a valid email address", { duration: 2000 });
+    } else if (e.target.validity.valueMissing) {
+      toast.error("Please enter your email address", { duration: 2000 });
+    }
+  };
+
+  const handlePasswordError = (e) => {
+    if (e.target.validity.typeMismatch) {
+      toast.error("Please enter a valid password", { duration: 2000 });
+    } else if (e.target.validity.valueMissing) {
+      toast.error("Please enter your password", { duration: 2000 });
     }
   };
 
@@ -49,18 +69,28 @@ function Login() {
             name="email"
             placeholder="Email"
             value={email}
+            onBlur={handleEmailError}
             className="px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
           />
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            className="px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
-          />
+          <div className="relative px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200">
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type={showPassword ? "text" : "password"}
+              value={password}
+              placeholder="Password"
+              onBlur={handlePasswordError}
+              className='border-none, outline-none'
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-2 text-gray-500 focus:outline-none"
+            >
+              {showPassword ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+            </button>
+          </div>
           <div className="flex gap-2 items-center justify-center">
-            <button onClick={handleEmailLogin} className="w-full px-4 py-2 text-white bg-green-600 rounded-xl transition duration-200 hover:bg-green-700">
+            <button type="submit" className="w-full px-4 py-2 text-white bg-green-600 rounded-xl transition duration-200 hover:bg-green-700">
               Login
             </button>
             <button onClick={handleGoogleLogin} className="flex items-center justify-center px-4 py-2 text-gray-500 bg-gray-100 rounded-xl transition duration-200 hover:bg-gray-200">
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,34 +1,46 @@
-// src/Signup.jsx
 import { Link } from 'react-router-dom';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../config/firebase-config';
 import { useState } from 'react';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const signup = async (e) => {
     e.preventDefault();
 
     // Check if passwords match
     if (password !== confirmPassword) {
-      alert("Passwords do not match");
+      toast.error("Passwords do not match", {
+        duration: 1000,
+      });
       return;
     }
 
     // Validate email and password
     if (!email || !password) {
-      alert("Email and password fields cannot be empty");
+      toast.error("Please fill in all fields", {
+        duration: 1000,
+      });
       return;
     }
 
     try {
       await createUserWithEmailAndPassword(auth, email, password);
-      alert("Account created successfully");
+      toast.success("Account created successfully");
     } catch (error) {
-      alert(`Account creation failed: ${error.message}`);
+      console.log(error);
+      toast.error("Account creation failed", {
+        duration: 1000,
+      });
     }
   }
 
@@ -44,20 +56,38 @@ function Signup() {
             placeholder="Email"
             className='px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200'
           />
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            value={password}
-            placeholder="Password"
-            className="px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
-          />
-          <input
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            type="password"
-            value={confirmPassword}
-            placeholder="Confirm Password"
-            className="px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
-          />
+          <div className="relative">
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type={showPassword ? "text" : "password"}
+              value={password}
+              placeholder="Password"
+              className="px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-3 text-gray-500 focus:outline-none"
+            >
+              {showPassword ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+            </button>
+          </div>
+          <div className="relative">
+            <input
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              type={showConfirmPassword ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              className="px-4 py-2 text-gray-700 border-2 border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+            />
+            <button
+              type="button"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              className="absolute right-3 top-3 text-gray-500 focus:outline-none"
+            >
+              {showConfirmPassword ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+            </button>
+          </div>
           <button
             type="submit"
             className="w-full px-4 py-2 text-white bg-green-600 rounded-xl transition duration-200 hover:bg-green-700"
